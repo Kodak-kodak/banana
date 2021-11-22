ac3e8451930d1029e6512893a3b9a5606f38ec3f
integerJI <jjs9536@gmail.com>
2021-11-22 22:51:36 +0900

    Test: branches dev

--- app_main/views.py
+++ app_main/views.py
@@ -4,3 +4,7 @@ from django.http import JsonResponse
 def index(request):
     data = {'email':'hansolove@gmail.com'}
     return JsonResponse(data)
+
+def index_test(request):
+    data = {'email':'hansolove@gmail.com', 'age':'20'}
+    return JsonResponse(data)
