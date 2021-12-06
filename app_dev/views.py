e487c89c1fa70df43ae1b8624059174277154f70
integerJI <jjs9536@gmail.com>
2021-12-06 21:56:52 +0900

    Feat: xhttp ==> fetch Update

--- app_dev/views.py
+++ app_dev/views.py
@@ -6,8 +6,16 @@ def index(request):
     return render(request, 'index.html')
 
 def test_json(request):
-    if request.method == 'POST':
+    if request.method == 'POST':    
+        print('===================')
+        print('POST')
+        print(request.POST)
+        print(request.POST.get)
+        print('===================')
         data = {'email':'test_post@gmail.com'}
     elif request.method == 'GET':
+        print('===================')
+        print('GET')
+        print('===================')
         data = {'email':'test_get@gmail.com'}
     return JsonResponse(data)
