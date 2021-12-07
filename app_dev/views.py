3f430e3785b426bd2204f2369c79e64b8aecd4de
integerJI <jjs9536@gmail.com>
2021-12-07 23:26:46 +0900

    Feat: CreateUserView validation add

--- app_dev/views.py
+++ app_dev/views.py
@@ -23,21 +23,29 @@ def test_json(request):
     return JsonResponse(data)
 
 class CreateUserView(View):
-
-    print('start CreateUserView !!!')
-
     def get(self, request):
         data = {'method':'get'}
         
         return JsonResponse(data)
 
     def post(self, request):
-        data = {'user_id':'test1', 'user_pw1':'test1', 'user_pw2':'test1'}
+        data = {'user_id':'test2', 'user_pw1':'test1', 'user_pw2':'test1'}
 
-        if data['user_pw1'] == data['user_pw2']: # 비밀번호 1과 2를 비교해 같을 경우 다음 함수 실행
-            user = User.objects.create_user(
-                username = data['user_id'],
-                password = data['user_pw1'],
-            )
+        if User.objects.filter(username=data['user_id']): 
+            print('이미 사용중인 아이디 입니다.')
+        else:
+            if data['user_pw1'] == data['user_pw2']: # 비밀번호 1과 2를 비교해 같을 경우 다음 함수 실행
+                
+                try:
+                    user = User.objects.create_user(
+                        username = data['user_id'],
+                        password = data['user_pw1'],
+                    )
+                    print('회원가입이 완료되었습니다.')
+                except KeyError:
+                    print('회원가입에 실패하였습니다.')
+
+            else :
+                print('비밀번호가 서로 다릅니다.')
 
         return JsonResponse(data)
