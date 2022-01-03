f83efba19b4925eec1a3e6c9fac3acd6fd81dfa6
integerJI <jjs9536@gmail.com>
2022-01-03 22:05:18 +0900

    Feat: dev app Initialization

--- app_dev/urls.py
+++ app_dev/urls.py
@@ -6,9 +6,4 @@ app_name = 'dev'
 # local dev test
 urlpatterns = [
     path('', views.index, name="index"),
-    path('test_json/', views.test_json, name="test_json"),
-    path('singup/', views.CreateUserView.as_view(), name='singup'),
-    path('main/', views.main, name='main'),
-    path('login/', views.login, name='login'),
-    path('logout/', views.logout, name='logout'),
 ]
