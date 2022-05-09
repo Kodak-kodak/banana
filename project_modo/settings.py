ffa885e88e8f87832d9e8a29ea63f1917a24ea9e
integerJI <jjs9536@gmail.com>
2022-05-09 22:16:34 +0900

    Feat: random secret key add

--- project_modo/settings.py
+++ project_modo/settings.py
@@ -11,32 +11,22 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 # secrets.json을 불러오기 위한 import
-import os, json
-from django.core.exceptions import ImproperlyConfigured
+import os
 
 from pathlib import Path
-
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-if os.path.isfile(os.path.join(BASE_DIR, 'secrets.json')) == True :
-    ################ LOCAL ################
-    secret_file = os.path.join(BASE_DIR, 'secrets.json')
+# ================= START =================
+# Get ascii Characters numbers and punctuation (minus quote characters as they could terminate string).
+# 원활한 로컬 테스트를 위한 랜덤 시크릿 키 생성 (임시)
+import string, random
 
-    with open(secret_file) as f:
-        secrets = json.loads(f.read())
+chars = ''.join([string.ascii_letters, string.digits, string.punctuation]).replace('\'', '').replace('"', '').replace('\\', '')
 
-    def get_secret(setting, secrets=secrets):
-        try:
-            return secrets[setting]
-        except KeyError:
-            error_msg = "Set the {} environment variable".format(setting)
-            raise ImproperlyConfigured(error_msg)
+RANDOM_SECRET_KEY = ''.join([random.SystemRandom().choice(chars) for i in range(50)])
 
-    SECRET_KEY = get_secret("SECRET_KEY")
-else :
-    ################ HEROKU ################
-    SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = RANDOM_SECRET_KEY
+# ================== END ==================
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
