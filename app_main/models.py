3c84e2cf92d54c34c0b69dae5522fa219d4966e1
integerJI <jjs9536@gmail.com>
2022-05-11 23:39:14 +0900

    Feat: Invitation Model Add

--- app_main/models.py
+++ app_main/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Invitation(models.Model):
+    invitation_title = models.CharField(max_length=200)
+    invitation_writer = models.CharField(max_length=50)
+    invitation_input_date = models.DateTimeField('date published')
+    invitation_content = models.TextField()
+
+    def __str__(self):
+        return '%s : %s' % (self.invitation_writer, self.invitation_title) 
--- app_main/views.py
+++ app_main/views.py
@@ -1,5 +1,51 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.http import JsonResponse
+from django.views import View
+from .models import Invitation
 
 def index(request):
-    return render(request, 'index.html')
+    invitations = Invitation.objects.all().order_by('-id')
+
+    context = {
+        'invitations' : invitations,
+    }
+
+    return render(request, 'index.html', context=context)
+
+
+class CreateInvitationView(View):
+    def get(self, request):
+        return render(request, 'create.html')
+
+    # def post(self, request):
+    #     response = {}
+        
+    #     body = request.body.decode('utf8')
+    #     data = json.loads(body)
+
+    #     if User.objects.filter(username=data['userid']): 
+    #         response["result"] = "false"
+    #         response["status_code"] = "500"
+    #         response["message"] = "이미 사용중인 아이디 입니다."
+    #         response["return_url"] = "/"
+    #         return JsonResponse(response, json_dumps_params = {'ensure_ascii': False}) # Line 69과 중복이 되는데 맞는걸까
+    #     else:
+    #         if data['password1'] == data['password2']: # 비밀번호 1과 2 비교
+    #             try:
+    #                 user = User.objects.create_user(
+    #                     username = data['userid'],
+    #                     password = data['password1'],
+    #                 )
+    #                 auth.login(request, user)
+    #                 return redirect('main:index')
+    #             except KeyError:
+    #                 response["result"] = "false"
+    #                 response["status_code"] = "200"
+    #                 response["message"] = "회원가입에 실패하였습니다."
+    #                 response["return_url"] = "/"
+    #         else :
+    #             response["result"] = "false"
+    #             response["status_code"] = "200"
+    #             response["message"] = "비밀번호가 서로 다릅니다."
+    #             response["return_url"] = "/"
+    #         return JsonResponse(response, json_dumps_params = {'ensure_ascii': False})
